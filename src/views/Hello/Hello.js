6491303385a2c3f2555b81d494c586326080355f
Evgeniy <aka47Hitman@gmail.com>
2017-10-24 00:25:27 +0500

    Add menu bar and logo

--- src/views/Hello/Hello.js
+++ src/views/Hello/Hello.js
@@ -1,30 +1,30 @@
 import Vue from 'vue';
 
-  export default {
-    name: 'hello',
+export default {
+  name: 'hello',
 
-    asyncData ({ store }) {
-      return store.dispatch('fetch', {
-        id: store.state.global.user._id,
-        endpoint: 'users',
-        namespace: 'user',
-        global: true
-      });
+  asyncData({ store }) {
+    return store.dispatch('fetch', {
+      id: store.state.global.user._id,
+      endpoint: 'users',
+      namespace: 'user',
+      global: true
+    });
+  },
+
+  computed: {
+    user() {
+      return this.$store.state.global.user;
     },
 
-    computed: {
-      user () {
-        return this.$store.state.global.user;
-      },
+    fullName() {
+      const name = this.$store.state.global.user.name;
 
-      fullName() {
-        const name = this.$store.state.global.user.name;
-
-        if (name) {
-          return name.first + ' ' + name.last;
-        }
-
-        return '';
+      if (name) {
+        return name.first + ' ' + name.last;
       }
+
+      return '';
     }
   }
+}
